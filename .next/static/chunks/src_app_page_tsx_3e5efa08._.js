16954e18270d88c97390eb6bfc8381b2b54dfd94
meltonjoshua <75980887+meltonjoshua@users.noreply.github.com>
2025-07-17 15:35:36 +0100

    Add .next/static/chunks/src_app_page_tsx_3e5efa08._.js 1752762935945

---
+++ .next/static/chunks/src_app_page_tsx_3e5efa08._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_16bf8741._.js",
+  "static/chunks/node_modules_6c6bcc28._.js"
+],
+    source: "dynamic"
+});
